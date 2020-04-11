536d0018b376f09fe7635e6399ca9b6ed0ad0cb2
ld8 <don_lee@me.com>
2020-04-11 23:07:08 +0800

    about page

--- src/Land.js
+++ src/Land.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Land.css";
-import { LandLink } from "./components/LandLink";
+import { About } from "./components/About";
 
 export const Land = () => {
-
   return (
-    <div>
-      <LandLink text={"ABOUT"}/>
-      <LandLink text={"PORTFOLIO"}/>
-      <LandLink text={"BLOG"}/>
+    <div className="container">
+      <div className="links">
+        <About />
+      </div>
+      <div className="bg-over"></div>
     </div>
-  )
-}
+  );
+};
--- src/components/LandLink.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const LandLink = (props) => {
-  return (
-    <div>
-      <a href="#">{props.text}</a>
-    </div>
-  );
-};
